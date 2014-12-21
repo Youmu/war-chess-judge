32813715fe8b523332dc9b2970d9c88303abeb36
Tengda Yan <yantd@qq.com>
2014-12-21 11:36:11 +0800

    modified:   CMakeLists.txt 	deleted:    arm.cmake

---
+++ arm/CMakeFiles/warchess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/warchess.dir/capture.o"
+  "warchess.pdb"
+  "warchess"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/warchess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
